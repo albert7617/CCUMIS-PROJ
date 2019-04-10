98eaa5e3cf7a3bc05cddd088e679c1e955c91f06
albert7617 <albert860728@gmail.com>
2019-04-10 16:25:02 +0800

    Query PDF.

--- app/src/main/java/com/example/albert/ccumis/QueryPDFTask.java
+++ app/src/main/java/com/example/albert/ccumis/QueryPDFTask.java
@@ -1,26 +1,31 @@
 package com.example.albert.ccumis;
 
-import android.app.Application;
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
+import android.os.Environment;
 import android.preference.PreferenceManager;
-import android.util.Log;
-import android.widget.Toast;
-
-import com.example.albert.ccumis.fragments.QueryFragment;
 
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Formatter;
 import java.util.HashMap;
 import java.util.Map;
 
 public class QueryPDFTask extends AsyncTask<Void, Void, Map<String, String>> {
+  @SuppressLint("StaticFieldLeak")
+  private Callback callback;
+  private Context context;
   private String serialNO;
   private int empType;
-  private Context context;
   public QueryPDFTask(Context context, String serialNo, int empType) {
     this.serialNO = serialNo;
     this.empType = empType;
@@ -30,34 +35,60 @@ public class QueryPDFTask extends AsyncTask<Void, Void, Map<String, String>> {
   @Override
   protected Map<String, String> doInBackground(Void... voids) {
     Map<String, String> postData = new HashMap<>(), retval = new HashMap<>();
-    postData.put("bsn", this.serialNO);
-    postData.put("emp_type", this.empType+"");
-    postData.put("go_check", "列印簽到退表");
-    postData.put("ctrow", "14");
+
     String sessid = login();
     if(sessid == null) {
       retval.put("error", "錯誤無法登入");
       return retval;
     }
+    postData.put("bsn", this.serialNO);
+    postData.put("emp_type", Integer.toString(this.empType));
+    postData.put("go_check", "列印簽到退表");
+    postData.put("sid", sessid);
     try {
       Connection.Response response;
-      response = Jsoup.connect(context.getString(R.string.url_prt_pdf))
-              .cookie("PHPSESSID", sessid)
-              .followRedirects(true)
-              .data(postData)
-              .data("sid", sessid)
-              .method(Connection.Method.POST)
-              .maxBodySize(0)
-              .ignoreContentType(true)
-              .execute();
-      Log.d("MIME", "doInBackground: "+response.contentType());
+      boolean flag = false;
+      for (int i = 1; i < 100; i++) {
+        postData.put("ctrow", Integer.toString(i));
+        response = Jsoup.connect(context.getString(R.string.url_prt_pdf))
+                .cookie("PHPSESSID", sessid)
+                .followRedirects(true)
+                .data(postData)
+                .method(Connection.Method.POST)
+                .maxBodySize(0)
+                .ignoreContentType(true)
+                .execute();
+        if(this.callback != null) {
+          this.callback.updateProgress(i);
+        }
+        if(response.contentType().contains("pdf")) {
+          flag = true;
+          writeToFile(response.bodyAsBytes());
+          break;
+        }
+        postData.remove("ctrow");
+      }
+      if(this.callback != null) {
+        this.callback.updateProgress(-1);
+      }
+      if(flag) {
+        return postData;
+      } else {
+        retval.put("error", "錯誤查無資料");
+        return retval;
+      }
     } catch (Exception e) {
       e.printStackTrace();
       retval.put("error", "未知錯誤");
       return retval;
     }
-    retval.put("error", "未知錯誤");
-    return retval;
+  }
+
+  @Override
+  protected void onPostExecute(Map<String, String> stringStringMap) {
+    if(this.callback != null) {
+      this.callback.result(stringStringMap);
+    }
   }
 
   private String login() {
@@ -84,5 +115,41 @@ public class QueryPDFTask extends AsyncTask<Void, Void, Map<String, String>> {
       // Connection error
       return null;
     }
+  }
+
+  private void writeToFile(byte[] data) throws Exception {
+
+    File root = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)+"/");
+    File file = new File(root, "ccu_mis_"+SHAsum(data)+".pdf");
+    if (!file.exists()) file.createNewFile();
+    FileOutputStream out = new FileOutputStream(file);
+    out.write(data);
+    out.close();
+    if(this.callback != null) {
+      callback.pdfSaved(file.getPath());
+    }
+  }
+
+  public static String SHAsum(byte[] convertme) throws NoSuchAlgorithmException {
+    MessageDigest md = MessageDigest.getInstance("SHA-1");
+    return byteArray2Hex(md.digest(convertme));
+  }
+
+  private static String byteArray2Hex(final byte[] hash) {
+    Formatter formatter = new Formatter();
+    for (byte b : hash) {
+      formatter.format("%02x", b);
+    }
+    return formatter.toString();
+  }
+
+  public interface Callback {
+    void result(Map<String, String> result);
+    void updateProgress(int progress);
+    void pdfSaved(String filePath);
+  }
+
+  public void setCallback (Callback callback) {
+    this.callback = callback;
   }
 }
--- app/src/main/java/com/example/albert/ccumis/fragments/QueryFragment.java
+++ app/src/main/java/com/example/albert/ccumis/fragments/QueryFragment.java
@@ -1,21 +1,33 @@
 package com.example.albert.ccumis.fragments;
 
-import android.app.Application;
+import android.Manifest;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.RadioGroup;
-import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.albert.ccumis.MainActivity;
 import com.example.albert.ccumis.QueryPDFTask;
 import com.example.albert.ccumis.R;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.Iterator;
+import java.util.Map;
 
 public class QueryFragment extends Fragment {
   // TODO: Rename parameter arguments, choose names that match
@@ -26,6 +38,13 @@ public class QueryFragment extends Fragment {
   // TODO: Rename and change types of parameters
   private String mParam1;
   private String mParam2;
+
+  private ProgressBar progressBar;
+  private TextView textView;
+  private CoordinatorLayout rootView;
+
+  private String bsn;
+  private int emp;
 
   public QueryFragment() {
     // Required empty public constructor
@@ -48,7 +67,6 @@ public class QueryFragment extends Fragment {
     fragment.setArguments(args);
     return fragment;
   }
-
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -57,19 +75,21 @@ public class QueryFragment extends Fragment {
       mParam2 = getArguments().getString(ARG_PARAM2);
     }
   }
-
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
     // Inflate the layout for this fragment
     View view = inflater.inflate(R.layout.fragment_query, container, false);
     final EditText editTextBSN = view.findViewById(R.id.pdf_batchNum);
     final RadioGroup radioGroup = view.findViewById(R.id.pdf_empTypeGroup);
+    progressBar = view.findViewById(R.id.progressBar);
+    textView = view.findViewById(R.id.querying);
+    rootView = view.findViewById(R.id.coordinator);
     Button submit = view.findViewById(R.id.pdf_submit);
     submit.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
-        String bsn = editTextBSN.getText().toString();
-        int emp = 0;
+        bsn = editTextBSN.getText().toString();
+        emp = 0;
         switch (radioGroup.getCheckedRadioButtonId()) {
           case R.id.emptype1:
             emp = 1;
@@ -81,10 +101,115 @@ public class QueryFragment extends Fragment {
             emp = 3;
             break;
         }
-        new QueryPDFTask(getContext(), "1080326061", 1).execute();
+        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+          // Permission is not granted
+          ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 200);
+        } else {
+          queryPDF();
+        }
 
       }
     });
     return view;
   }
+
+  private void queryPDF() {
+    progressBar.setVisibility(View.VISIBLE);
+    textView.setVisibility(View.VISIBLE);
+    progressBar.setMax(99);
+    progressBar.setProgress(0);
+    QueryPDFTask task = new QueryPDFTask(getContext(), bsn, emp);
+    task.setCallback(callback);
+    task.execute();
+  }
+
+  @Override
+  public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+    switch (requestCode) {
+      case 200: {
+        // If request is cancelled, the result arrays are empty.
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+          // permission was granted, yay! Do the
+          // contacts-related task you need to do.
+          queryPDF();
+        } else {
+          // permission denied, boo! Disable the
+          // functionality that depends on this permission.
+        }
+        return;
+      }
+
+      // other 'case' lines to check for other
+      // permissions this app might request.
+    }
+  }
+
+  private QueryPDFTask.Callback callback = new QueryPDFTask.Callback() {
+    @Override
+    public void result(Map<String, String> result) {
+      if(result.containsKey("error")) {
+        Toast.makeText(getContext(), result.get("error"), Toast.LENGTH_SHORT).show();
+      } else {
+        StringBuilder stringBuilder = new StringBuilder();
+        Iterator iterator = result.entrySet().iterator();
+        while (iterator.hasNext()) {
+          Map.Entry entry = (Map.Entry) iterator.next();
+          stringBuilder.append(entry.getKey());
+          stringBuilder.append("=");
+          try {
+            stringBuilder.append(URLEncoder.encode(String.valueOf(entry.getValue()), "UTF-8"));
+          } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+          }
+
+          if(iterator.hasNext()){
+            stringBuilder.append("&");
+          }
+        }
+
+      }
+    }
+
+    @Override
+    public void updateProgress(int progress) {
+      if(progress == -1){
+        getActivity().runOnUiThread(new Runnable() {
+          @Override
+          public void run() {
+            progressBar.setVisibility(View.GONE);
+            textView.setVisibility(View.GONE);
+          }
+        });
+      } else {
+        progressBar.setProgress(progress);
+      }
+    }
+
+    @Override
+    public void pdfSaved(final String filePath) {
+      getActivity().runOnUiThread(new Runnable() {
+        @Override
+        public void run() {
+          Snackbar.make(rootView, "工讀單已儲存到下載資料夾", Snackbar.LENGTH_SHORT)
+                  .setAction("開啟", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                      Intent target = new Intent(Intent.ACTION_VIEW);
+                      target.setDataAndType(Uri.parse(filePath),"application/pdf");
+                      Intent intent = Intent.createChooser(target, "Open File");
+                      try {
+                        startActivity(intent);
+                      } catch (ActivityNotFoundException e) {
+                        // Instruct the user to install a PDF reader here, or something
+                      }
+                    }
+                  })
+                  .show();
+        }
+      });
+    }
+  };
+
+
 }
